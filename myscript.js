232d62017007c62586c79a74d8959d03eeef8aea
Rui Condesso <ruicondesso98@gmail.com>
2023-09-12 21:36:09 +0100

    Initial commit

---
+++ myscript.js
@@ -0,0 +1,11 @@
+let  password = "couves";
+
+
+
+alert("Loggin")
+var pass = prompt("Password: ")
+
+if(pass == password){
+    var element = document.getElementById("hide");
+    var hidden = element.removeAttribute("hidden");
+}
